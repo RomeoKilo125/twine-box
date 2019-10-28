57f7cf0b0e004af5dc71204c62fef9e1c750bc26
RomeoKilo125 <romeokilo125@gmail.com>
2019-10-27 21:42:34 -0400

    refactored for JS Standard

--- advent2018/day02.js
+++ advent2018/day02.js
@@ -1,12 +1,12 @@
-let input =
+const input =
 ['uosnxmfkezbojfdgwvrtaqhluy', 'iosnxmfkazbcopdgnvrtaqhluy', 'ioanxmfkezbcjpdgwvrjaohluy', 'uosnxmfkezbcjpjgwvrtaqhlut', 'imsnxmfkezbcjpugwvataqhluy', 'ioenxmfkezbcjpdgwvrraqhluz', 'iosnxmfkezbcjpdgevitnqhluy', 'iosnxmfkezcccpdgcvrtaqhluy', 'loinxmfkezbcjpdgwvrtaqhluu', 'iosnlmfkezbczndgwvrtaqhluy', 'iosnxmfkezbcjpdgwvrifghluy', 'iosnuhfkezbcjpugwvrtaqhluy', 'iosnxmfkezbcwpdgwvrtaihlgy', 'iosnxzfwuzbcjpdgwvrtaqhluy', 'hosnxmfjizbcjpdgwvrtaqhluy', 'iornxmfktzbcjpdgwvrtaqhluo', 'nosnxmfkdzbcjpdgwvrtaqhlwy', 'iosnxmfkezbcjpdgwvrtaktluq', 'ioszxmlkezbcjvdgwvrtaqhluy', 'ionnxmfkezbcfpdgwvbtaqhluy', 'iosnxmfkezrcjedgwvrtaqhluq', 'irsnxmfkezbcjpdqwvrtafhluy', 'ioshxmzkezbccpdgwvrtaqhluy', 'iosnxmfkezbrjpdgwvothqhluy', 'bosnxmfkezbcbpdgwvrtnqhluy', 'iosnomfkszbcjpcgwvrtaqhluy', 'iosnxmflezbcjpdgwvrtaqmuuy', 'iobnxmfkezbcjpdgxvrtaqfluy', 'ioenxmfvezbcjgdgwvrtaqhluy', 'iosnxmfkekbcjprgwvrtaqhlty', 'iosnumfkezbcjpmgwvrtaqhlmy', 'ionnxufkezbcjpdgwvrqaqhluy', 'tosnxmfbezbcjpdghvrtaqhluy', 'iosnxmfktzbcjpogwmrtaqhluy', 'iosnamfkezbjjpdgtvrtaqhluy', 'iosnemfkezmcjpdgwvrtaqhlry', 'losnxmfkezbcjpdxwvrtaqsluy', 'fomnxmekezbcjpdgwvrtaqhluy', 'rosnxmfkezbcjzdcwvrtaqhluy', 'iosngmrkezbcjpdgwvrtaqhduy', 'iosnxmaaebbcjpdgwvrtaqhluy', 'xosnxmfkezbcjpdgwvrmrqhluy', 'iosnxmfkgzbujpdgwhrtaqhluy', 'iosnxmekecbcjpmgwvrtaqhluy', 'mesnxmfdezbcjpdgwvrtaqhluy', 'insnxmbkezbcjpdgwvrtgqhluy', 'iosyxmfkezbcjpdgwirtavhluy', 'iosnxmfkezbcjpdgwlvtjqhluy', 'iosnxmtkezbcjpdgwvjtaqhlut', 'iosnxmfkezbsjpdhwvrtaqaluy', 'iosnumfkezbcjpfgwvrtaqhlfy', 'iosnxmekezbcdpxgwvrtaqhluy', 'iosnfmfkezbcjpdgavctaqhluy', 'iosnxmfkezvcjpdgfvrtamhluy', 'iovnxmfkezbcjpdgzvrtaqhzuy', 'iosnbmfkuzbcjpdgwvrtaqhlux', 'iosnxmfkezbcjpdgwvftauhluc', 'iosmbmfkezbcmpdgwvrtaqhluy', 'ifsnxmfvezbcjpdgwvrwaqhluy', 'iosnxmfkezfcjpdgwvrmaqhyuy', 'iospxmfkezbcjpdkwvytaqhluy', 'issnxmfkyzbcjpdgwyrtaqhluy', 'iosnxmfkezbcjpdbwvrtjqhluz', 'iosnxmfkwzbcjpdgfvrtajhluy', 'iosnxmfkezbcjndgwvrnaqxluy', 'iosnxmfkezbcjpdgwvltawwluy', 'iosnxmfkezbcjpdguvrtwqhtuy', 'iornxmfkezbcjpdgwertaihluy', 'iofdxmokezbcjpdgwvrtaqhluy', 'iosnxmfkezbcjpdgwgrtiqdluy', 'iosnxmfkenbcjpdgwqrtiqhluy', 'iosnxmfkezbcjpugwvotcqhluy', 'iksnxmfkezbcjfdgqvrtaqhluy', 'iasnxmfkezbcjpdgwvrtaqheuo', 'iosnxmfkehbcipdgwvrtaqtluy', 'iosnxifkezbajpdgwvrtaahluy', 'iosnxmpkezbccpdguvrtaqhluy', 'ioinxnfkezbcjpdgwvgtaqhluy', 'gosnxmfkezbcjpdgwvrtvqkluy', 'iolnxmfcezbcjpdgwvrtaqhlgy', 'iosnxmfkezbcppdgwortjqhluy', 'iesnxafkezbcjpdgwvrtayhluy', 'iqsnxmfxazbcjpdgwvrtaqhluy', 'cosnxmfkezbcjpdgwvrtkahluy', 'ioenxmfkezbcjpdgwvrtzqyluy', 'iosnxmhkwzbcjpdgwvrtabhluy', 'iosnxmfkezbcjpdtwvrhaqiluy', 'iisnxmfkezbcvpdwwvrtaqhluy', 'iosnsmfkeobcjpdgfvrtaqhluy', 'iwsnxmfkfzbcjpugwvrtaqhluy', 'iosnxmflezbcjpdgwvrtaspluy', 'gosnimfkezbcjpdgwvrtjqhluy', 'iosnxmfkfibcjmdgwvrtaqhluy', 'iosnxmfkpzbcjpdgwvitaqhwuy', 'ionnxmfkerbcjpjgwvrtaqhluy', 'iosnxmfkezecjgdgwvrtaqhljy', 'iosnxufkezbcjpdguvltaqhluy', 'vosnzmfkezbcjpdvwvrtaqhluy', 'iolnxmfkecbcjpdgwvrtaqpluy', 'iosnxmfkezbcjpdgwortaqhouw', 'iomnxmfkezbckpdgwvrtaqhluu', 'iopnymfkezbchpdgwvrtaqhluy', 'iosnxmfkezhcjpdguvrtaqhnuy', 'iosfxmfkezecjpdgyvrtaqhluy', 'iopnxmfkgzbcjpdgwvbtaqhluy', 'tosnxmffezbcjpdgwvttaqhluy', 'iosnxmfkpabcjpdywvrtaqhluy', 'iosrxmfkekbcjpdgwvrtaqrluy', 'iosnxmokezbcjpdjwvrtaxhluy', 'iolnxmfkezbccpdgwvetaqhluy', 'iosnxmfketecjpdgwvrtaqnluy', 'iosnxmfkxzbtjpdgwvroaqhluy', 'ioinxmfkezbcjpdqwvrtjqhluy', 'iosnxmfkqzbcjpdgwvrtaqzluz', 'iosnxmfklzbcjpdgwwrtaqhluh', 'iosnxmfkezbcjpdtwvrtmqhlpy', 'iosnomfqezgcjpdgwvrtaqhluy', 'iosnxmfkezbcjodgwvutaqhduy', 'iosnxmfkezbcjppgwertaqhluu', 'iosnxmfkezbcjqdggvrtaqhluw', 'iosnxmvkezbcjpdgwvrtlqfluy', 'icsnwmfkezbcjpdiwvrtaqhluy', 'iosnxxbkezbhjpdgwvrtaqhluy', 'ioknxmfkezacjpdgwvrtaqhliy', 'iosgxmfkezbcjpdgevrtpqhluy', 'iosnxmfkezbejpdgwlrtaqhldy', 'iosnxyfkezbcjpdowvrtaqhlur', 'iosnxmfkezbcjpnjwvrtaqhlvy', 'iosnxglkezbcjpdvwvrtaqhluy', 'iosnxmpkezbcjpdgwvrtxqhlub', 'iosnxsfwezbcjpdgwmrtaqhluy', 'aosnxmfkezbcjpdgwvrtaqhpwy', 'iopnxmqkkzbcjpdgwvrtaqhluy', 'iosnxmfkewbcfpdgwvrtaqmluy', 'iosnxmfkekbcjpdgwvltawhluy', 'iosnxmfmezbcjpdgwvitaqtluy', 'iosnomfkezbcjpggwvrtaqhlly', 'iobnkmfkezbcjpdywvrtaqhluy', 'yosnxmfkezbcjydgwvrtarhluy', 'iosnxifkezbckpdgyvrtaqhluy', 'iornxmfkezbcjpduwvreaqhluy', 'ivsfxmfjezbcjpdgwvrtaqhluy', 'iosnxmfkezbcspdgwartaqhlui', 'iosnxmfkezbcjpdgasstaqhluy', 'iosnxmfkezbajpdgwvrtaqmlux', 'gzsnxmfkezxcjpdgwvrtaqhluy', 'iosnxmikczbcjpdgwvrtyqhluy', 'iosnxmgkezbcjvdgwdrtaqhluy', 'iosnxpfkezbcjpdgwvrbachluy', 'igsnxmfkezbcjpdgwkrtaqtluy', 'posnxmfkfzbcjpdgwvrpaqhluy', 'iosnxmfkezbhjtdgwvrtaqhsuy', 'iosfxmfkezbcjpdwwvrtaqvluy', 'iosnxmfkehecjpdgwvrtaqoluy', 'iasnxmfkezbckpdgfvrtaqhluy', 'iosnxmfkezbwjpdggvrtaqhlmy', 'iosnxmfkezbcjpdgwvrkaqhbun', 'iosnxmikezbcjpdgwvrtaqhlnt', 'iosnxmfiazbcjpdgwvetaqhluy', 'iosnxmfkczbcjpfgwvrnaqhluy', 'iosnxmfkezkcjpdgsvrqaqhluy', 'iosnxmfkezbcspdgwvrtaqhxuc', 'iosnxmfdezbcjpdgwzrteqhluy', 'qosnxmrkezbcjpdgwvrtaqhlpy', 'iosnxmfkpabcjpdywvrtawhluy', 'ojsnxmfkezbcjpdgwvrtiqhluy', 'iosrxmfkezbcjpdgdvrtaqhlmy', 'iosnxmfkezbcnqdgwvrtayhluy', 'ionnxmfkezbcjpdgwvrsaehluy', 'iosnxmfkezbcjpdgwvrtmqhpuk', 'ifsnxmfkezbcjpdpwvrtaqhluf', 'insnxmfkezbcjpdgwrrtaqhmuy', 'iosnxmfxezbcjpdjwvrfaqhluy', 'iojnxmbkezccjpdgwvrtaqhluy', 'iosnomlkezbcjpdgwvotaqhluy', 'iosnamfkezbcjpdgwvrhqqhluy', 'iksnxmfkezbbjrdgwvrtaqhluy', 'iosnfmfkezbcjpdgwvrtaqhyay', 'iosnxmzkezbcjpdayvrtaqhluy', 'iosnxmfkezbcwpdgwbrtaqhlut', 'iosnxmfkezccjpdgivrtaqhbuy', 'iosuxmfkezbcjgdgwvrtaqhvuy', 'ipsnxmfkezbcjpaiwvrtaqhluy', 'iisnxmfkezbcjpdgpvrtaqqluy', 'ihsnxmfkezbcspdgwvrtahhluy', 'imsnxmfkezbcjpdgwvrtaqhkly', 'josnxmfkezbpjpdgwvttaqhluy', 'bosnxyfkezmcjpdgwvrtaqhluy', 'iosnxmfkezbcjpkgwvrtkqhjuy', 'iosnxmfkezbcjpdgwfrgaqfluy', 'rosnxmfkqzbcjpdgwvxtaqhluy', 'iosnxmfkezbcjpdgwlrwaqhluu', 'yysnxmfkezbcjpdgwvrtaxhluy', 'iosnxmpkezbcjldgwvrtaqoluy', 'gosnxmfkezrcjpdgwvrtarhluy', 'iosnxmfrezbcjrdmwvrtaqhluy', 'iosnxmfkekbcjpdgpvrtaqhyuy', 'iosbemfkezbcjpdgwdrtaqhluy', 'iosnxmfkezucjpdgwvatamhluy', 'ioanfmfkwzbcjpdgwvrtaqhluy', 'iosnxphkezbcjpdgwvrtaqhlly', 'ioynxmfkezbcjvbgwvrtaqhluy', 'iosnnmfkwzbcjpdgwvrtaqbluy', 'iosnxmfjezbcjpkgwtrtaqhluy', 'iosexmfkezbcjpdgwvrtmshluy', 'irsnxmwkezbcjpdgwvotaqhluy', 'iosnxmfkezpcjpdgwvrlaqkluy', 'iosnxmfkezbcjpwgwvroaqkluy', 'iosnxmfkizbcjpdgwvrtaqxlay', 'ioszxmfkezbcjpdgwertrqhluy', 'iosnxmfkczscjpdgwvrtcqhluy', 'iosnxmfkedbcjpdgwirtaqhliy', 'iosgxmfpezbcjpdgwvvtaqhluy', 'iownxmfiezbcjpdgwvrtajhluy', 'iosnxmfkezbejudgwvrqaqhluy', 'iomnpmfkezbcjpdgwvwtaqhluy', 'ioshxmfkecbcjpdgwfrtaqhluy', 'iosnxmfkezmcjpdgwzrtaqkluy', 'iownxdfkezdcjpdgwvrtaqhluy', 'iosnxmfjezbcjpdgwrotaqhluy', 'roknxmfkezbcjpdgwxrtaqhluy', 'iosnxmfkeibcjpdgovrtaqhloy', 'ifsnxmfkelbcjpdgwvrcaqhluy', 'iosnamfuezbcjpdwwvrtaqhluy', 'rssnxmfkeebcjpdgwvrtaqhluy', 'iosnomfkjzbcjpdgwvrtaqhlun', 'iosnxmfuezbcjpdgwfjtaqhluy', 'iosnxzfkezbcjpdewvrtaqhlfy', 'iosnxmfkezbcjpdgwvrtzqhlgr', 'iosixmfkezbcjpdgwvrkaqhlut', 'issnxmfkezbdjpdpwvrtaqhluy', 'iosnxmfrezbcjpdgwkrtaghluy', 'iysnxmfkezbcjpdgwrrtmqhluy', 'iosoxmfkezbcjpdgwjrtaqhlua', 'eosnxmfkezvcjpdgwvztaqhluy', 'iosmxmckezbcjpdgwvrtaqhlay', 'iosnxmfkezbcjodgwvrtaqhlma', 'josnxwftezbcjpdgwvrtaqhluy', 'iosnxjfkepbcjpdgwvrtaqhlsy', 'iosnnmfkezbcjpdgwvriaqhnuy', 'iosnxofkezbcupdgwvrtayhluy', 'iosnxmfkezbcjpddwvroaqhluz', 'iosnomfkezbcapdhwvrtaqhluy', 'iosixmfkezycjpdgwvrtaqhruy', 'iosnwefkezbcjpdgwvrtaqcluy', 'iosnxmfkvzbcbpdgwvrhaqhluy', 'insnxmfkezbczpdgwvrtajhluy', 'iosnxrfkelbcjpdgwvrtaqhluf', 'iosnxmfkezbcjpdgwsrtaqhzud', 'iosnxmfyvzbcjpdgwyrtaqhluy']
 
-//Part One
-let exactlyTwo = [],
-  exactlyThree = []
+// Part One
+const exactlyTwo = []
+const exactlyThree = []
 
 input.forEach((e, i, a) => {
-  let counts = {}
+  const counts = {}
   e.split('').forEach((e, i, a) => {
     if (counts[e]) {
       counts[e]++
@@ -14,13 +14,13 @@ input.forEach((e, i, a) => {
       counts[e] = 1
     }
   })
-  for (let char in counts) {
+  for (const char in counts) {
     if (counts[char] === 2) {
       exactlyTwo.push(e)
       break
     }
   }
-  for (let char in counts) {
+  for (const char in counts) {
     if (counts[char] === 3) {
       exactlyThree.push(e)
       break
@@ -28,16 +28,16 @@ input.forEach((e, i, a) => {
   }
 })
 
-// console.log(exactlyTwo.length * exactlyThree.length)
+console.log(exactlyTwo.length * exactlyThree.length)
 
-//Part Two
+// Part Two
 
-function countMismatches(str1, str2) {
+function countMismatches (str1, str2) {
   if (str1.length !== str2.length) {
     return 'Length Disparity'
   }
   let count = 0
-  for (i = 0; i < str1.length; i++) {
+  for (let i = 0; i < str1.length; i++) {
     if (str1[i] !== str2[i]) {
       count++
     }
@@ -45,12 +45,12 @@ function countMismatches(str1, str2) {
   return count
 }
 
-function findSimilarIDs(arr) {
-  let pairs = []
+function findSimilarIDs (arr) {
+  const pairs = []
   for (let i = 0; i < arr.length; i++) {
-    let test1 = arr[i]
+    const test1 = arr[i]
     for (let j = i + 1; j < arr.length; j++) {
-      let test2 = arr[j]
+      const test2 = arr[j]
       if (countMismatches(test1, test2) === 1) {
         pairs.push([test1, test2])
       }
