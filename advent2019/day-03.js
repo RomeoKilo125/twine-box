a2c7dae38979049a03a4cac332c9d0313733cbe6
RomeoKilo125 <romeokilo125@gmail.com>
2019-12-08 01:35:14 -0500

    finding the max distance in each direction

--- advent2019/day-03.js
+++ advent2019/day-03.js
@@ -52,10 +52,31 @@ const test1b = ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83']
 const test2a = ['R98', 'U47', 'R26', 'D63', 'R33', 'U87', 'L62', 'D20', 'R33', 'U53', 'R51']
 const test2b = ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7']
 
-function createPanelFromPaths(path1, path2) {
-  
+function createPanelFromPaths (path1, path2) {
+  const directions = ['L', 'R', 'D', 'U']
+  const dimensions = {
+    L: '0',
+    R: '0',
+    D: '0',
+    U: '0'
+  }
+  directions.forEach(dir => {
+    const dirVal = Math.max(...path1.filter(e => e.includes(dir)).map(e => e.replace(dir, '')))
+    if (dirVal > dimensions[dir]) {
+      dimensions[dir] = dirVal
+    }
+  })
+  directions.forEach(dir => {
+    const dirVal = Math.max(...path2.filter(e => e.includes(dir)).map(e => e.replace(dir, '')))
+    if (dirVal > dimensions[dir]) {
+      dimensions[dir] = dirVal
+    }
+  })
+  console.log(dimensions)
 }
 
 function plotWire (wire, array) {
 
 }
+
+createPanelFromPaths(test1a, test1b)
