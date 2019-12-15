4aa3afdfbc46c88472482d6c9e022ced43ed5ca5
RomeoKilo125 <romeokilo125@gmail.com>
2019-12-14 23:45:07 -0500

    function to calulate distance

--- advent2019/day-03.js
+++ advent2019/day-03.js
@@ -143,6 +143,8 @@ function plotWire (wire, array) {
   })
 }
 
+function calculateDistance (pointA, pointB) {
+  return Math.abs(pointA[0] - pointB[0]) + Math.abs(pointA[1] - pointB[1])
 }
 
 let panel = createPanelFromPaths(test1a, test1b)
